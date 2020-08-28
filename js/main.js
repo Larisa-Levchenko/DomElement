bd67cacd5d26f33a4a88a5aed930638be8f1f29e
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-08-28 21:05:39 +0700

    feat lesson14-extra in js

--- js/main.js
+++ js/main.js
@@ -12,19 +12,41 @@ DomElement.prototype.addElement = function () {
     let elem;
     if (this.selector.charAt(0) === '.') {
         elem = document.createElement('div');
-        elem.classList.add(this.selector);
+        elem.classList.add(this.selector.substring(1, this.selector.length));
     }
     if (this.selector.charAt(0) === '#') {
         elem = document.createElement('p');
-        elem.id = this.selector;
+        elem.id = this.selector.substring(1, this.selector.length);
     }
     elem.style.height = `${this.height}px`;
     elem.style.width = `${this.width}px`;
     elem.style.backgroundColor = `#${this.bg}`;
-    elem.style.fontSize = `${this.fontSize}px`;
-    elem.textContent = 'Урок №14';
+    elem.style.fontSize = `${this.fontSize}px`;   
     document.body.append(elem);
 };
 
-let element = new DomElement('#block', 100, 200, '268927', 35);
+let element = new DomElement('#block', 100, 100, '268927', 35);
 element.addElement();
+const block = document.querySelector('#block');
+block.style.position='fixed';
+
+let vertically = 0;
+let horizontally = 0;
+document.body.addEventListener('keypress', function () {   
+    if (event.keyCode === 50) {
+        vertically += 10;
+    }
+    if (event.keyCode === 56) {
+        vertically -= 10;
+    }
+    if (event.keyCode === 54) {
+        horizontally += 10;        
+    }
+    if (event.keyCode === 52) {
+        horizontally -= 10;
+    }
+    block.style.top = `${vertically}px`;
+    block.style.left = `${horizontally}px`;
+});
+
+
